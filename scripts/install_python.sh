8727c6abf2c97670f91d9ec0a95113c53b79e5de
m-ceta <moto@PCISDM0193.IMAZO.local>
2020-12-04 17:47:28 +0900

    anything

--- scripts/install_python.sh
+++ scripts/install_python.sh
@@ -6,28 +6,28 @@ pyenv install 3.8.1
 pyenv install 2.7.18
 #make virtualenv
 pyenv global 3.8.1
+pip install --upgrade pip
 pip install virtualenv
 virtualenv -p python3 ~/nvim-python3
 pyenv global 2.7.18
+pip install --upgrade pip
 pip install virtualenv
 virtualenv -p python ~/nvim-python2
 pyenv global system
 #install requirement
 source ~/nvim-python3/bin/activate
+pip install --upgrade pip
 pip install pynvim
-pip install python-language-server
-pip install pyls-isort
-pip install pyls-black
-pip install pyflakes
-pip install fortran-language-server
 pip install neovim
 deactivate
 source ~/nvim-python2/bin/activate
+pip install --upgrade pip
 pip install pynvim
+pip install neovim
+deactivate
+pip install --upgrade pip
 pip install python-language-server
 pip install pyls-isort
 pip install pyls-black
 pip install pyflakes
 pip install fortran-language-server
-pip install neovim
-deactivate
