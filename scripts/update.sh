cc900d9b50cf551b48e488500f2c11ae53d28db7
m-ceta <moto@PCISDM0193.IMAZO.local>
2021-10-18 13:40:52 +0900

    anything

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-pass=$(sh ~/dotfiles/scripts/viewtoken.sh)
-echo "PASS: "$pass
+user=m-ceta\n
+pass=$(sh ~/dotfiles/scripts/viewtoken.sh)\n
 cd ~/dotfiles
 git add .
 git commit -m "anything"
-git push origin master
+git push origin master < $user < $pass
