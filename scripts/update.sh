d70fea252ac61e71e00221e31d400feffb2a763e
m-ceta <moto@PCISDM0193.IMAZO.local>
2021-10-18 12:24:20 +0900

    anything

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
+user=m-ceta
+pass=$(sh ~/dotfiles/scripts/viewtoken.sh)
+auth="${user}\n${pass}\n"
 cd ~/dotfiles
 git add .
 git commit -m "anything"
-git push origin master
+git push origin master < echo -e $auth 
