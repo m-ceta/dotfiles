1e85ec6f8085f641a23bbd685780b8163016f018
m-ceta <motohiro.seta@gmail.com>
2024-11-26 18:53:58 +0900

    Delete etc directory

--- etc/wxformbuilder.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-set WXBUILDER="D:\Program Files\wxFormBuilder\wxFormBuilder.exe"
-
-python %~d0%~p0wxformbuilder.py %1 %WXBUILDER%
--- etc/wxformbuilder.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-WXBUILDER=wxFormBuilder
-python3 $(dirname ${0})/wxformbuilder.py $[1] $WXBUILDER
